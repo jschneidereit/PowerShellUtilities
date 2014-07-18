4f512e311fcafd9f4f261b62016c3b26b940215a
t0rqued <jimschneidereit@gmail.com>
2014-07-18 14:34:11 -0700

    updated autologon

---
+++ MDT/Clear-OldWIMFiles.ps1
@@ -0,0 +1,7 @@
+﻿$TargetDirectory = "D:\blah\blah\*"
+$TargetExtension = "*.wim"
+$TargetAge = "90"
+
+$Comparison = (Get-Date).AddDays(-$TargetAge)
+
+Get-ChildItem -Path $TargetDirectory -Include $TargetExtension | ? { $_.LastWriteTime -le $Comparison } | Remove-Item -Force
--- MDT/Enable-AutoLogonWinPE.ps1
+++ MDT/Enable-AutoLogonWinPE.ps1
@@ -12,36 +12,48 @@
 # Requirements Cont.: Place a target script in your script root in MDT, or disable related lines of code if you just want the autologon functionality
 #*=============================================
 
-#region Configuration Variables
-$Localadmin = "Administrator"
-$Localadminpass = "localadminpasswordhere"
-$ScriptName = "runscripthere.ps1"
-$ExecutionPolicy = "Unrestricted"
-$FileName = "ITUtilities"
-#endregion
-
-#biggest assumption of script (that you are in the "C" drive) fix later
 REG LOAD HKLM\MNTSOFTWARE D:\Windows\system32\config\SOFTWARE
 REG LOAD HKLM\MNTSYSTEM D:\Windows\system32\config\SYSTEM
 
-if (!(Test-Path D:\$FileName)) { New-Item -Path D:\ -Name $FileName -ItemType Directory} else { continue }
+$localadminpass = ""
+$localComputer = (Get-ItemProperty HKLM:\MNTSYSTEM\ControlSet001\Control\ComputerName\ComputerName).computername
+$localDirectory = "ITUtilities"
+$copyScript = "runLiteTouch.ps1"
+$exPolicy = "Unrestricted"
 
-Copy-Item -Path $tsenv:deployroot\Scripts\$ScriptName -Destination D:\$FileName -Force
+$LogonHash = @{ 
+    AutoAdminLogon = [pscustomobject]@{ Name = "AutoAdminLogon"; Value = "1"; Type = "String" }
+    ForceAutoLogon = [pscustomobject]@{ Name = "ForceAutoLogon"; Value = "1"; Type = "String" }
+    DefaultDomainName = [pscustomobject]@{ Name = "DefaultDomainName"; Value = $localComputer; Type = "String" }
+    DefaultUserName = [pscustomobject]@{ Name = "DefaultUserName"; Value = "Administrator"; Type = "String" }
+    DefaultPassword = [pscustomobject]@{ Name = "DefaultPassword"; Value = $localadminpass; Type = "String" }
+    AutoLogonCount = [pscustomobject]@{ Name = "AutoLogonCount"; Value = 5; Type = "DWord" }
+}
 
-$LocalComputer = (Get-ItemProperty HKLM:\MNTSYSTEM\ControlSet001\Control\ComputerName\ComputerName).computername
+function Configure-RegistryProperty($Node, $Name, $Value, $Type) { 
+    if (!(Get-ItemProperty $Node -Name $Name -ErrorAction SilentlyContinue)) {
+        New-ItemProperty $Node -Name $Name -Value $Value -Type $Type -ErrorAction SilentlyContinue -Force
+    } else {
+        Set-ItemProperty $Node -Name $Name -Value $Value -Type $Type -ErrorAction SilentlyContinue -Force
+    }
+}
 
-#Force admin auto logon
-Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value "1" -Type String -Force
-Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name ForceAutoLogon -Value "1" -Type String -Force
-#Set admin auto logon credentials
-Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultDomainName -Value $LocalComputer -Type String -Force
-Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value $Localadmin -Type String -Force
-#Generate and set admin password
-New-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value $Localadminpass -Type String -ErrorAction SilentlyContinue -Force
-Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value $Localadminpass -Type String -ErrorAction SilentlyContinue -Force
-#Sets count of allowed auto logon times
-New-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 5 -Type DWord -ErrorAction SilentlyContinue -Force
-#Set autorun command for script that was copied from deployment share
-New-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name RunLiteTouch -Value "C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command `"`& `'C:\$FileName\$ScriptName`'`"" -Force
 
-Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\PowerShell\1\ShellIds\ScriptedDiagnostics' -Name ExecutionPolicy -Value $ExecutionPolicy -ErrorAction SilentlyContinue -Force
+if (!(Test-Path "D:\$localDirectory")) { New-Item -Path D:\ -Name $localDirectory -ItemType Directory} else { continue }
+Copy-Item -Path "$tsenv:deployroot\Scripts\$copyScript" -Destination "D:\$localDirectory" -Force
+Set-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\PowerShell\1\ShellIds\ScriptedDiagnostics' -Name ExecutionPolicy -Value $exPolicy -ErrorAction SilentlyContinue -Force
+New-ItemProperty 'HKLM:\MNTSOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name RunLiteTouch -Value "C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command `"`& `'C:\$localDirectory\$copyScript`'`"" -Force
+
+if (Test-Path HKLM:\MNTSOFTWARE\ -ErrorAction SilentlyContinue) {
+    $32Node = 'HKLM:\MNTSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+    $LogonHash.GetEnumerator() | % { Configure-RegistryProperty -Node $32Node -Name $_.Value.Name -Value $_.Value.Value -Type $_.Value.Type }
+} else { 
+    Continue
+}
+
+if (Test-Path HKLM:\MNTSOFTWARE\Wow6432Node\ -ErrorAction SilentlyContinue) {
+    $64Node = 'HKLM:\MNTSOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Winlogon'
+    $LogonHash.GetEnumerator() | % { Configure-RegistryProperty -Node $64Node -Name $_.Value.Name -Value $_.Value.Value -Type $_.Value.Type }
+} else { 
+    Continue
+}

4e4c91531ee1c95aa5d787ab7d552f431e8d9076
GameOver69 <sopranosmainman@yahoo.com>
2014-08-18 09:48:37 -0400

    Update Clear-OldWIMFiles.ps1
    Rewrote and cleaned up.

--- MDT/Clear-OldWIMFiles.ps1
+++ MDT/Clear-OldWIMFiles.ps1
@@ -1,7 +1,19 @@
-﻿$TargetDirectory = "D:\blah\blah\*"
-$TargetExtension = "*.wim"
-$TargetAge = "90"
+##Script to Delete .WIM Files older than 90 Days##
 
-$Comparison = (Get-Date).AddDays(-$TargetAge)
+# Set Folder Path
+$TargetDirectory = "c:\blah blah\*"
 
-Get-ChildItem -Path $TargetDirectory -Include $TargetExtension | ? { $_.LastWriteTime -le $Comparison } | Remove-Item -Force
+# Set File Extension
+$TargetExtension = "*.ext"
+
+# Set Minimum Age of Files
+$Max_Age = "-90"
+
+# Get the Current Date
+$CurrentDate = Get-Date
+
+# Determine How Far Back From Current Date to Delete
+$DateToDelete = $CurrentDate.AddDays($Max_Age)
+
+# Deletes Files Older than Days Specified Above
+Get-ChildItem -include $TargetExtension $TargetDirectory | Where-Object { $_.LastWriteTime -lt $DateToDelete } |Where-Object { -not ($_.psiscontainer) } | Foreach-Object {Remove-Item $_.FullName}

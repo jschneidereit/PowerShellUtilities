32945b235a07745faa6adc789768d1f38f824ab9
Jim Schneidereit <jimschneidereit@gmail.com>
2015-01-28 07:45:02 -0800

    beginning of personal new computer image script

---
+++ Misc/New-PCSoftware.ps1
@@ -0,0 +1,5 @@
+﻿$utilities = @("filezilla", "githubforwindows", "virtualbox", "svn")
+$ides = @("intellijidea-community", "pycharm-community", "texmaker", "miktex", "sublimetext3", "visualstudiocommunity2013")
+$comms = @("trillian", "skype")
+$dev = @("python2", "lein") #pythonxy not in chocolatey :(
+$misc = @("steam", "teamviewer")

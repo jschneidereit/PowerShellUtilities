64f051a083bf4bb68fc3782be8620582cc7745eb
Jim Schneidereit <jimschneidereit@gmail.com>
2015-02-27 16:20:56 -0800

    Added Show Database

---
+++ Misc/Show-Database.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string]$Server = ".",
+    [string]$DBName = "Name",
+    [string]$Table = "TName"
+)
+
+$ConnectionString = "Server = $Server; Database = $DBName; Integrated Security = True"
+$Query = "SELECT * from $Table"
+
+$Connection = New-Object System.Data.SqlClient.SqlConnection
+$Connection.ConnectionString = $ConnectionString
+
+$DataSet = New-Object System.Data.DataSet
+$Command = New-Object System.Data.SqlClient.SqlCommand
+$Adapter = New-Object System.Data.SqlClient.SqlDataAdapter
+$Command.CommandText = $Query
+$Command.Connection = $Connection
+$Adapter.SelectCommand = $Command
+$Adapter.Fill($DataSet)
+$DataSet.Tables

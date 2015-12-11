ee012e75313d9080385ed3764f0eb0cba49cf361
Schneidereit <schnejam@oregonstate.edu>
2015-12-11 12:00:47 -0800

    Byah! updated some stuff

--- MDT/Deploy-ReferenceVirtualMachine.ps1
+++ MDT/Deploy-ReferenceVirtualMachine.ps1
@@ -10,59 +10,19 @@
 # Purpose: Automates reference (generic) image creation
 # Intendend: As scheduled event on WDS/MDT server - monthly to take advantage of WSUS updates
 # Requirements: PowerShell V3.0 or higher, MDT and associated softwares
-# Note: Does not "import" the wim, and as such might not work well with task sequence, true importing is trivial but does not allow for zero human interface
+# Note: Does not "import" the wim, and as such might not work well with task sequence, true importing is trivial but does not allow for any human interface
 #*=============================================
 
 <#   Will restart script as "run as administrator" - DO NOT MODIFY   #>
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{   
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-    Start-Process powershell -Verb runAs -ArgumentList $arguments
-   Break
+
+Function Assert-AdminInstance {
+    If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {   
+        $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+        Start-Process powershell -Verb runAs -ArgumentList $arguments
+       Break
+    }
 }
-
-#region Configurable Variables
-
-$DeploymentShareDirectory = "C:\DeploymentShares"
-$VirtualMachineDirectory = "C:\Virtual Machines"
-
-<#   Reference Depoyment Share   #>
-$RDS = @{
-    Path = "$DeploymentShareDirectory\Reference"
-    Name = "Reference"
-}
-
-<#   Target Deployment Share   #>
-$TDS = @{
-    Path = "$DeploymentShareDirectory\Target"
-    Name = "Target"
-    OSFileName = "GENERIC"
-}
-
-<#   Virtual Machine Settings   #>
-$VM = @{
-    Path = "$VirtualMachineDirectory"
-    Name = "GENERIC"
-    RAM = 2048MB
-    VHDSize = 128GB
-    <#   This switch will be created if it doesn't exist   #>
-    Switch = "Virtual Switch"
-    <#   This is the iso that the RDS generates when you "update deployment share"   #>
-    ISO = "$($RDS.Path)\Boot\LiteTouchPE_x64.iso"
-}
-
-#implement later
-<#   MDT Administrators - for emailing reports   #>
-$MDTAdministrators = @{
-    "server" = "user.name@domain.com"
-    "server2" = "user.name@domain.com"
-}
-
-#endregion
-
-$ScriptVersion = "1"
-$VHDPath = "$($VM.Path)\$($VM.Name).vhdx"
-$LogPath = "$DeploymentShareDirectory\Logs\"
 
 
 <#   Use this to cleanup mess from other functions   #>
@@ -78,8 +38,8 @@ Function Clean-SpecifiedLocation {
 }
 
 <#   Creates a virtual switch for the VM to use to connect   #>
-Function New-VirtualSwitch {
-    If (!(Get-NetAdapter | ? {($_.Name -like "vEthernet ($($VM.Switch))")})) {
+Function New-VirtualSwitch($vmswitchname) {
+    If (!(Get-NetAdapter | ? {($_.Name -like "vEthernet ($vmswitchname)")})) {
         $Adapters = Get-NetAdapter -Physical | ? {($_.Status -eq 'Up')}
         if ($Adapters.count) {
             $AvailableAdapters = $Adapters.GetEnumerator() | % { $_.name }
@@ -89,11 +49,11 @@ Function New-VirtualSwitch {
         }
     }
     try {
-        New-VMSwitch -Name $VM.Switch -NetAdapterName $Adapter -AllowManagementOS $true -ErrorAction Stop -ErrorVariable $temperror
-        Write-Log "A new virtual switch $($VM.Switch) has been generated"
+        New-VMSwitch -Name $vmswitchname -NetAdapterName $Adapter -AllowManagementOS $true -ErrorAction Stop -ErrorVariable $temperror
+        Write-Log "A new virtual switch $vmswitchname has been generated"
     } catch [Microsoft.HyperV.PowerShell.VirtualizationOperationFailedException] {  
-        if (Get-NetAdapter | % {$_.Name -match $VM.Switch}) { 
-            Write-Log "A virtual switch with the name $($VM.Switch) had already been generated, continuing on"
+        if (Get-NetAdapter | % {$_.Name -match $vmswitchname}) { 
+            Write-Log "A virtual switch with the name $vmswitchname had already been generated, continuing on"
         } else {
            Write-Log "Something horrible happened - Couldn't add the virtual switch, the rest of this script will not complete successfully and will exit"
         }
@@ -103,48 +63,46 @@ Function New-VirtualSwitch {
 }
 
 <#   Removes the virtual switch created for the VM   #>
-Function Remove-VirtualSwitch {
-    If ( Get-NetAdapter | ? {($_.Name -like "vEthernet ($($VM.Switch))")} ) {
-        Write-Log "Removing the switch: $($VM.Switch)"
-        Remove-VMSwitch -Name $VM.Switch -Force
+Function Remove-VirtualSwitch($vmswitchname) {
+    If ( Get-NetAdapter | ? {($_.Name -like "vEthernet ($vmswitchname)")} ) {
+        Write-Log "Removing the switch: $vmswitchname"
+        Remove-VMSwitch -Name $vmswitchname -Force
     } else {
-        Write-Log "There wasn't a switch by the name $($VM.Switch)"
+        Write-Log "There wasn't a switch by the name $vmswitchname"
     }
 }
-
-    
-
 
 <#   Creates a new virtual machine to run the task sequence, defined in $VM hastable   #>
-Function New-VirtualMachine {
-    
-    If (!(Get-VM | % {$_.Name -match $VM.Name} )) {
-        Write-Log "Creating the new Virtual Machine named $($VM.Name) on $env:ComputerName"
-        New-VM -Name $VM.Name -BootDevice CD -MemoryStartupBytes $VM.RAM -SwitchName $VM.Switch -Path $VM.Path -NoVHD -Verbose
+Function New-VirtualMachine($vmname, $vmram, $vmswitch, $vmpath, $vhdpath, $vhdsize, $vmiso) {
+   
+    New-VirtualSwitch -vmswitchname $vmswitch
+
+    If (!(Get-VM | % {$_.Name -match $vmname} )) {
+        Write-Log "Creating the new Virtual Machine named $vmname on $env:ComputerName"
+        New-VM -Name $VM.Name -BootDevice CD -MemoryStartupBytes $vmram -SwitchName $vmswitch -Path $vmpath -NoVHD -Verbose
     } else {
-        Write-Log "A Virtual Machine name $($VM.Name) already exists on $env:ComputerName"
+        Write-Log "A Virtual Machine name $vmname already exists on $env:ComputerName"
     }
 
-    If (!(Get-VHD -Path $VHDPath -ErrorAction SilentlyContinue)) {
-        Write-Log "Creating a new VHD at $VHDPath"
-        New-VHD -Path $VHDPath -SizeBytes $VM.VHDSize -Verbose
+    If (!(Get-VHD -Path $vhdpath -ErrorAction SilentlyContinue)) {
+        Write-Log "Creating a new VHD at $vhdpath"
+        New-VHD -Path $vhdpath -SizeBytes $vhdsize -Verbose
     } else {
-        Write-Log "A VHD already exists at $VHDPath removing and then rebuilding"
-        Remove-Item $VHDPath -Force
-        Write-Log "Creating a new VHD at $VHDPath"
-        New-VHD -Path $VHDPath -SizeBytes $VM.VHDSize -Verbose
+        Write-Log "A VHD already exists at $vhdpath removing and then rebuilding"
+        Remove-Item $vhdpath -Force
+        Write-Log "Creating a new VHD at $vhdpath"
+        New-VHD -Path $vhdpath -SizeBytes $vhdsize -Verbose
     }
 
-    Add-VMHardDiskDrive -VMName $VM.Name -Path $VHDPath -Verbose
-    Set-VMDvdDrive -VMName $VM.Name -Path $VM.ISO -Verbose
-    Start-VM -VMName $VM.Name
+    Add-VMHardDiskDrive -VMName $vmname -Path $VHDPath -Verbose
+    Set-VMDvdDrive -VMName $vmname -Path $vmiso -Verbose
+    Start-VM -VMName $vmname
 }
 
-
 <#   Waits for the virtual machine defined in the $VM hashtable   #>
-Function Wait-VirtualMachine {
+Function Wait-VirtualMachine($vmname) {
     Write-Log "The virtual machine is running, waiting for it to complete"
-    $VirtualMachine = Get-VM -Name $VM.Name
+    $VirtualMachine = Get-VM -Name $vmname
     
     While ($VirtualMachine.State -ne "off") {
         Sleep -Seconds 120
@@ -154,21 +112,20 @@ Function Wait-VirtualMachine {
 }
 
 <#   Removes the virtual machine defined in the $VM hashtable   #>
-Function Remove-VirtualMachine {
-    Write-Log "Removing the virtual machine $($VM.Name)"
-    Remove-VM -Name $VM.Name -Force
-    Remove-Item -Path "$($VM.Path)\$($VM.Name)" -Recurse -Force
-    Remove-Item -Path "$($VM.Path)\$($VM.Name).vhdx" -Recurse -Force 
-    #Remove-VirtualSwitch
+Function Remove-VirtualMachine($vmname, $vmpath) {
+    Write-Log "Removing the virtual machine $($vmname)"
+    Remove-VM -Name $vmname -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "$vmpath\$vmname" -Recurse -Force  -ErrorAction SilentlyContinue
+    Remove-Item -Path "$vmpath\$vmname.vhdx" -Recurse -Force  -ErrorAction SilentlyContinue
 }
 
 <#   Copies the captured image from the VM task sequence to the Target Deployment Share   #>
-Function Copy-CapturedImage {
+Function Copy-CapturedImage($ReferencePath, $TargetPath, $TargetOSFileName) {
     Write-Host "Beginning the copy of captured image"
     Write-Host "This will clobber whatever image was already there..."
     
-    $Capture = (Get-ChildItem ($RDS.Path + "\Captures")).FullName
-    $Destination =  ($TDS.Path + "\Operating Systems\" + $TDS.OSFileName + "\" + $TDS.OSFileName)
+    $Capture = (Get-ChildItem ($ReferencePath + "\Captures")).FullName
+    $Destination =  ($TargetPath + "\Operating Systems\" + $TargetOSFileName + "\" + $TargetOSFileName)
 
     If (Test-Path -Path ($Destination  + ".WIM")) {
         If (Test-Path -Path ($Destination + ".old")) {Remove-Item ($Destination + ".old") -Verbose}
@@ -176,7 +133,7 @@ Function Copy-CapturedImage {
     }
 
     If (Test-Path -Path $Capture) {
-        Copy-Item -Path $Capture -Destination ($TDS.Path + "\Operating Systems\" + $TDS.OSFileName) -Force -Verbose
+        Copy-Item -Path $Capture -Destination ($TargetPath + "\Operating Systems\" + $TargetOSFileName) -Force -Verbose
         If ($?) { 
             Write-Log "Copy successful!"
             Remove-Item $Capture 
@@ -189,43 +146,93 @@ Function Copy-CapturedImage {
 }
 
 <#   Builds log file if necessary   #>
-Function Build-Log {
+Function Build-Log($LogPath) {
     $LogFile = ($LogPath + "VMLog_" + (Get-Date -Format s).Replace(":","") + ".txt")
     if (!(Test-Path -Path $LogFile)) { New-Item -Path $LogFile -ItemType File -Force} else { continue }
     Return $LogFile
 }
 
 <#   Initializes the log file   #>
-Function Initialize-Log {
+Function Initialize-Log($LogFile, $ScriptVersion) {
      "`nStarted processing at [$(Get-Date)]. Running script version [$ScriptVersion]." | Out-File -FilePath $LogFile -Append -Force
 }
 
 <#   Writes the message with a time stamp to the log file   #>
-Function Write-Log {
-    Param ( $Message )
-
+Function Write-Log($Message) {
     $FormatedMessage = "`n$(Get-Date -Format t)  -  $Message"
     Write-Verbose $FormatedMessage
     #$FormatedMessage | Out-File -FilePath $LogFile -Append -Force
 }
 
 <#   Ends the log   #>
-Function Complete-Log {
+Function Complete-Log($LogFile) {
     "`nCompleted processing at [$(Get-Date)]." | Out-File -FilePath $LogFile -Append -Force
 }
 
-#$LogFile = Build-Log
-#Initialize-Log
-if (!(Get-Module -Name Hyper-V)) {Import-Module -Name Hyper-V}
-Write-Host "hello good sir"
-Clean-SpecifiedLocation ($RDS.Path + "\Captures")
-Write-Host "gonna do virtual switch"
-New-VirtualSwitch
-Write-Host "gonna do virtual machine"
-New-VirtualMachine
-Write-Host "gonna wait..."
-Wait-VirtualMachine
-Write-Host "gonna copy captured image"
-Copy-CapturedImage
-Remove-VirtualMachine
-#Complete-Log 
+
+#region Configurable Variables
+
+
+
+
+#endregion
+
+Function New-DeploymentShare($Path, $Name, $OSFileName)
+{
+    $DS = New-Object PSObject
+    $DS | Add-Member -NotePropertyName Path -NotePropertyValue $Path
+    $DS | Add-Member -NotePropertyName Name -NotePropertyValue $Name
+    $DS | Add-Member -NotePropertyName OSFileName -NotePropertyValue $OSFileName
+
+    return $DS
+}
+
+
+Function New-VMObject($Path, $Name, $RAM, $VHDSize, $Switch, $DeploymentShare)
+{
+    $VM = New-Object PSObject
+    $VM | Add-Member -NotePropertyName Path -NotePropertyValue $Path
+    $VM | Add-Member -NotePropertyName Name -NotePropertyValue $Name
+    $VM | Add-Member -NotePropertyName RAM -NotePropertyValue $RAM
+    $VM | Add-Member -NotePropertyName VHDSize -NotePropertyValue $VHDSize
+    $VM | Add-Member -NotePropertyName VHDPath -NotePropertyValue "$($Path)\$($Name).vhdx"
+    $VM | Add-Member -NotePropertyName Switch -NotePropertyValue $Switch
+    <#   This is the iso that MDT generates when you "update deployment share"   #>
+    $VM | Add-Member -NotePropertyName ISO -NotePropertyValue "$($DeploymentShare.Path)\Boot\LiteTouchPE_x64.iso"
+
+    return $VM
+}
+
+Function MDT-TestBuild($VM)
+{
+    if (!(Get-Module -Name Hyper-V)) {Import-Module -Name Hyper-V}
+    New-VirtualMachine -vmname $vmname -vmram $vmram -vmswitch $VM.Switch -vmpath $VM.Path -vhdpath $VHDPath -vhdsize $VM.VHDSize -vmiso $VM.ISO
+}
+
+Function MDT-TestCleanup($VM)
+{
+    Remove-VirtualMachine -vmname $vmname -vmpath $VM.Path
+}
+
+Function Execute-VMBuild($VM)
+{
+    Assert-AdminInstance
+    if (!(Get-Module -Name Hyper-V)) {Import-Module -Name Hyper-V}
+
+    Clean-SpecifiedLocation($RDS.Path + "\Captures")
+
+    New-VirtualMachine -vmname $vmname -vmram $vmram -vmswitch $VM.Switch -vmpath $VM.Path -vhdpath $VHDPath -vhdsize $VM.VHDSize -vmiso $VM.ISO
+    Remove-VirtualMachine -vmname $vmname -vmpath $VM.Path
+}
+
+
+$ScriptVersion = "1"
+$LogPath = "$DeploymentShareDirectory\Logs\"
+$DeploymentShareDirectory = "C:\DeploymentShares"
+$VirtualMachineDirectory = "C:\Virtual Machines"
+
+$RDS = New-DeploymentShare -Name "Reference" -Path "$DeploymentShareDirectory\Reference" -OSFileName "GENERIC"
+$RVM = New-VMObject -Path $VirtualMachineDirectory -Name "GENERIC" -RAM 2048MB -VHDSize 64GB -Switch "Virtual Switch" -DeploymentShare $TDS
+
+$TDS = New-DeploymentShare -Name "Target" -Path "$DeploymentShareDirectory\Target" -OSFileName "GENERIC"
+$TVM = New-VMObject -Path $VirtualMachineDirectory -Name "GENERIC" -RAM 2048MB -VHDSize 64GB -Switch "Virtual Switch" -DeploymentShare $TDS
---
+++ MDT/Remove-Win10Apps.ps1
@@ -0,0 +1,12 @@
+﻿$targets = @("3dbuilder", "windowsalarms", "windowscalculator",
+             "windowscommunicationsapps", "windowscamera", "officehub",
+             "skypeapp", "getstarted", "zunemusic", "windowsmaps",
+             "solitairecollection", "bingfinance", "zunevideo",
+             "bingnews", "onenote", "people", "windowsphone", "photos",
+             "windowsstore", "bingsports", "soundrecorder", "bingweather",
+             "xboxapp")
+
+ForEach ($target in $targets) {
+    $app = "*$target*"
+    Get-AppxPackage $app | Remove-AppxPackage -ErrorAction SilentlyContinue
+}
